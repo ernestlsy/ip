25b7710a4afdb8eefcb6494cb75b73a8539183de
ernestlsy <e.lsy0902@gmail.com>
2025-01-24 05:14:58 +0800

    Added echo functionatlity.

--- src/main/java/Pixel.java
+++ src/main/java/Pixel.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
 public class Pixel {
     public static void main(String[] args) {
         String greet = "________________________________ \n"
-                + " Hello! I'm Pixel \n"
-                + " What can I do for you? \n"
+                + " Hello! I'm your personal assistant, Pixel \n"
+                + " How may I assist you? \n"
                 + "________________________________ \n";
-        String bye = " Bye. Hope to see you again soon! \n"
+        String bye = "________________________________ \n"
+                + " Goodbye. Hope to see you again soon! \n"
                 + "________________________________ \n";
+        System.out.println(greet);
 
-        System.out.println(greet + bye);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(bye);
+                sc.close();
+                break;
+            } else {
+                System.out.println("________________________________ \n"
+                        + input + "\n________________________________ \n");
+            }
+        }
     }
 }
