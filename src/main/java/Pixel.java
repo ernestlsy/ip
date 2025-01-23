b728d6a463cfbfe40ac2795f196cac4ad6dc1c06
ernestlsy <e.lsy0902@gmail.com>
2025-01-24 04:59:33 +0800

    Renamed Duke to Pixel. Added greeting and exit message.

---
+++ src/main/java/Pixel.java
@@ -0,0 +1,12 @@
+public class Pixel {
+    public static void main(String[] args) {
+        String greet = "________________________________ \n"
+                + " Hello! I'm Pixel \n"
+                + " What can I do for you? \n"
+                + "________________________________ \n";
+        String bye = " Bye. Hope to see you again soon! \n"
+                + "________________________________ \n";
+
+        System.out.println(greet + bye);
+    }
+}
