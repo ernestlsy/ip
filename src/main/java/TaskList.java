f8c6bfaea64cc5a2918ea6fdb4b0fb4ea87a33c3
ernestlsy <e.lsy0902@gmail.com>
2025-01-30 17:33:15 +0800

    Enable automatically saving task list contents
    Contents of task list are lost after program terminates
    
    It would be better to have records of the task list history after termination
    
    Let's write the contents of the task list to a file
    (path: "./data/Pixel.txt") after every update to the task list
    
    That way, the task list contents will be saved and can be retrieved after program termination

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class TaskList {
     private ArrayList<Task> contents = new ArrayList<>(100);
 
     protected String addTask(Task task) {
         this.contents.add(task);
+        saveToFile();
         return String.format("Got it. I've added this task:\n  %s\nNow you have %d tasks in the list", task.toString(), this.contents.size());
     }
 
@@ -22,6 +27,7 @@ public class TaskList {
             throw new PixelException("Please input a valid task number!");
         }
         this.contents.get(idx).markTask();
+        saveToFile();
         return "Excellent, I've marked this task as done:\n  " + this.contents.get(idx);
     }
     protected String unmarkTask(int idx) throws PixelException {
@@ -29,6 +35,7 @@ public class TaskList {
             throw new PixelException("Please input a valid task number!");
         }
         this.contents.get(idx).unmarkTask();
+        saveToFile();
         return "Alright, I've marked this task as not done yet:\n  " + this.contents.get(idx);
     }
 
@@ -37,6 +44,21 @@ public class TaskList {
             throw new PixelException("Please input a valid task number!");
         }
         Task task = this.contents.remove(idx);
+        saveToFile();
         return String.format("No problem, I've removed the task from the list:\n  %s\nNow you have %d tasks in the list", task.toString(), this.contents.size());
+    }
+
+    private void saveToFile() {
+        try {
+            File directory = new File("./data");
+            directory.mkdir();
+            File file = new File("./data/Pixel.txt");
+            file.createNewFile();
+            FileWriter writer = new FileWriter("./data/Pixel.txt");
+            writer.write(this.toString());
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
     }
 }
