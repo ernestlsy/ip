38de0aa1da3fe6654e42c511a46065a1b3ada373
ernestlsy <e.lsy0902@gmail.com>
2025-02-06 21:40:06 +0800

    Add unit testing for Parser and TaskList classes

--- src/main/java/pixel/util/PixelException.java
+++ src/main/java/pixel/util/PixelException.java
@@ -1,11 +1,12 @@
 package pixel.util;
 
 public class PixelException extends RuntimeException {
+    private static final PixelException UNKNOWN_INPUT_EXCEPTION = new PixelException("Sorry, I'm not sure what that means...");
     public PixelException(String message) {
         super(message);
     }
 
     public static PixelException unknownInput() {
-        return new PixelException("Sorry, I'm not sure what that means...");
+        return PixelException.UNKNOWN_INPUT_EXCEPTION;
     }
 }
---
+++ src/test/java/pixel/util/ParserTest.java
@@ -0,0 +1,64 @@
+package pixel.util;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import pixel.command.AddCommand;
+import pixel.command.UpdateCommand;
+import java.time.LocalDateTime;
+
+
+public class ParserTest {
+
+    @Test
+    public void testParseFullCommand() {
+        // valid AddCommand
+        assertInstanceOf(AddCommand.class, Parser.parseFullCommand("deadline /by 2024-12-30 12:00"));
+
+        // valid UpdateCommand
+        assertInstanceOf(UpdateCommand.class, Parser.parseFullCommand("mark 0"));
+
+        // invalid index for UpdateCommand
+        try {
+            Parser.parseFullCommand("mark x");
+            fail();
+        } catch (PixelException e) {
+            assertEquals("Please input a valid task number!", e.getMessage());
+        }
+
+        // invalid command input
+        try {
+            Parser.parseFullCommand("hello");
+            fail();
+        } catch (PixelException e) {
+            assertEquals(PixelException.unknownInput(), e);
+        }
+    }
+
+    @Test
+    public void testParseDateTime() {
+        // valid dateTime with time
+        assertEquals(LocalDateTime.parse("2024-12-30T12:35"), Parser.parseDateTime("2024-12-30 12:35"));
+
+        // valid dateTime without time
+        assertEquals(LocalDateTime.parse("2024-09-25T00:00"), Parser.parseDateTime("2024-09-25"));
+
+        // invalid dateTime
+        try {
+            Parser.parseDateTime("2021-05-1k 12:00");
+            fail();
+        } catch (PixelException e) {
+            assertEquals("Please use the format YYYY-MM-DD HH:MM for the date and time!", e.getMessage());
+        }
+
+        // wrong format
+        try {
+            Parser.parseDateTime("2021/07/15 12:00");
+            Parser.parseDateTime("2021-04-17 05.29");
+            fail();
+        } catch (PixelException e) {
+            assertEquals("Please use the format YYYY-MM-DD HH:MM for the date and time!", e.getMessage());
+        }
+    }
+}
